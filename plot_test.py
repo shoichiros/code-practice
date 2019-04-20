7fef969cb248742e242adc3f0c4239cb86c72a0b
ss <46625673+shoichiros@users.noreply.github.com>
2019-04-20 21:48:17 +0900

    change

--- plot_test.py
+++ plot_test.py
@@ -1,7 +1,9 @@
 import matplotlib.pyplot as plt
-import math
 import numpy as np
-y=np.array([50,60,100,80,75,100])
-x=np.array([0,20,40,60,80,100])
-plt.plot(x,y,color="red")
+
+# Number List
+x = np.array([0, 20, 40, 60, 80, 100])
+y = np.array([50, 60, 100, 80, 75, 100])
+
+plt.plot(x, y, color="red")
 plt.show()
