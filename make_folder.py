bced8c563726d0a3f878b0df7dae6c0eaad8f6cd
ss <46625673+shoichiros@users.noreply.github.com>
2019-04-21 05:54:35 +0900

    change

--- make_folder.py
+++ make_folder.py
@@ -1,16 +1,17 @@
 import os
 
+
 def makefolderfile(maindir):
-    for mknumber in range(1,2):
+    for mknumber in range(1, 2):
         dircss = maindir + '/' + maindir + str(mknumber) + '/templates'
         dirimg = maindir + '/' + maindir + str(mknumber) + '/static'
 
-        os.makedirs(dircss,exist_ok=True)
-        os.makedirs(dirimg,exist_ok=True)
-        with open(maindir + '/' + maindir + str(mknumber) + '/templates/index.html','w'):
-            with open(maindir + '/' + maindir + str(mknumber) + '/static/style.css','w'):
+        os.makedirs(dircss, exist_ok=True)
+        os.makedirs(dirimg, exist_ok=True)
+        with open(maindir + '/' + maindir + str(mknumber) + '/templates/index.html', 'w'):
+            with open(maindir + '/' + maindir + str(mknumber) + '/static/style.css', 'w'):
                 mknumber += 1
 
-main = input('新規フォルダ名を指定してください')
 
+main = input('新規フォルダ名を指定してください')
 makefolderfile(main)
